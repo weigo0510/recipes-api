fb8f53ca24614c53d6ce1e9d4358decda7fa0003
wayneg021 <wayne.gong@farfetch.com>
2023-01-03 10:50:35 +0800

    update to support JWT authentication

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"context"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
+	"net/http"
 	"os"
 	"recipes-api/handlers"
 )
 
 var recipesHandler *handlers.RecipesHandler
+var authHandler *handlers.AuthHandler
 
 func init() {
 	//recipes := make([]models.Recipe, 0)
@@ -35,6 +38,7 @@ func init() {
 		DB:       0,
 	})
 	recipesHandler = handlers.NewRecipesHandler(ctx, collection, redisClient)
+	authHandler = &handlers.AuthHandler{}
 	//var lRecipes []interface{}
 	//for _, r := range recipes {
 	//	lRecipes = append(lRecipes, r)
@@ -49,8 +53,17 @@ func init() {
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if c.GetHeader("X-API-KEY") != os.Getenv("X_API_KEY") {
-			c.AbortWithStatus(401)
+		tokenVal := c.GetHeader("Authorization")
+		claims := &handlers.Claims{}
+		tkn, err := jwt.ParseWithClaims(tokenVal, claims,
+			func(token *jwt.Token) (interface{}, error) {
+				return []byte(os.Getenv("JWT_SECRET")), nil
+			})
+		if err != nil {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+		if tkn == nil || !tkn.Valid {
+			c.AbortWithStatus(http.StatusUnauthorized)
 		}
 		c.Next()
 	}
@@ -60,6 +73,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/recipes", recipesHandler.ListRecipesHandler)
 	//4router.GET("/recipes/search", SearchRecipesHandler)
+	router.POST("/signin", authHandler.SignInHandler)
 	authorized := router.Group("/")
 	authorized.Use(AuthMiddleware())
 	{
